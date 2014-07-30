8354e59309cf80c6c70f89989e629fd58527b028
Kenneh <kenneh_ftw@live.com>
2014-07-29 21:43:37 -0400

    Moved API package

--- src/org/liquidbot/bot/api/context/ClientContext.java
+++
@@ -1,7 +0,0 @@
-package org.liquidbot.bot.api.context;
-
-/**
- * Created by Kenneth on 7/29/2014.
- */
-public class ClientContext {
-}
--- src/org/liquidbot/bot/api/wrappers/NPC.java
+++
@@ -1,7 +0,0 @@
-package org.liquidbot.bot.api.wrappers;
-
-/**
- * Created by Kenneth on 7/29/2014.
- */
-public class NPC {
-}

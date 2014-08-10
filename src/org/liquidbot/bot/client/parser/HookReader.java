5a236d6a77e2b82aa24521e9fbf7a4fab83b500e
Kenneh <kenneh_ftw@live.com>
2014-08-10 13:40:23 -0400

    Reverted offline testing changes

--- src/org/liquidbot/bot/client/parser/HookReader.java
+++ src/org/liquidbot/bot/client/parser/HookReader.java
@@ -12,7 +12,7 @@ public class HookReader {
     public static Hashtable<String, FieldHook> fields = new Hashtable<>();
     public static Hashtable<String, MethodHook> methods = new Hashtable<>();
 
-    private static final String HOOKS_URL = "http://pastebin.com/raw.php?i=Eum62RBN";
+    private static final String HOOKS_URL = "https://www.liquidbot.org/hooks.html";
 
     /**
      * Parse Html File to get hooks info

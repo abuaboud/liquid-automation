797b561037503f47acd3dd2d2ba72f8437444d69
Kenneh <kenneh_ftw@live.com>
2014-08-11 10:58:46 -0400

    Script Settings directory

--- src/org/liquidbot/bot/script/api/util/Environment.java
+++ src/org/liquidbot/bot/script/api/util/Environment.java
@@ -2,6 +2,8 @@ package org.liquidbot.bot.script.api.util;
 
 import org.liquidbot.bot.Configuration;
 
+import java.io.File;
+
 
 /*
  * Created by Hiasat on 8/3/14
@@ -14,5 +16,12 @@ public class Environment {
 
     public static String getDisplayName() {
         return Configuration.getInstance().getUser().getDisplayName();
+    }
+
+    public static File getStorageDirectory() {
+        final File file = new File(System.getProperty("user.home")  + File.separator + "LiquidBot" + File.separator + "Script Settings");
+        if(!file.exists())
+            file.mkdirs();
+        return file;
     }
 }

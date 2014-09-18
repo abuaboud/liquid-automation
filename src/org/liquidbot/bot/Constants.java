f00021a3ea43d9580c3ee2ee904933f483b030d3
Magorium <magorium@shadowbot.org>
2014-09-18 14:09:36 +0300

    Update 0.15
    Update 0.15

--- src/org/liquidbot/bot/Constants.java
+++ src/org/liquidbot/bot/Constants.java
@@ -8,7 +8,7 @@ import java.io.File;
  */
 public class Constants {
 
-	public static final double CLIENT_VERSION = 0.14;
+	public static final double CLIENT_VERSION = 0.15;
 
 	public static final String CLIENT_TITLE = "LiquidBot Automation";
 
